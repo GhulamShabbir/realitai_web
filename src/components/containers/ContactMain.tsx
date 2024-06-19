a24429988a921e45ebaa9a4444296c3dfb82c964
GhulamShabbir <ghulamshabir1234567@gmail.com>
2024-06-19 18:45:19 +0500

    New Project

---
+++ src/components/containers/ContactMain.tsx
@@ -0,0 +1,157 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import phone from "public/images/phone.png";
+import mail from "public/images/mail.png";
+import location from "public/images/location.png";
+import time from "public/images/time.png";
+
+const ContactMain = () => {
+  return (
+    <section className="section contact-m fade-wrapper">
+      <div className="container">
+        <div className="row gaper">
+          <div className="col-12 col-sm-6 col-xl-3">
+            <div className="contact-m__single topy-tilt fade-top">
+              <div className="thumb">
+                <Image src={phone} alt="Image" />
+              </div>
+              <div className="content">
+                <h4>Phone & Fax</h4>
+                <p>
+                  <Link href="tel:197-90-56-780">Mobile : +92-3335512815</Link>
+                </p>
+                <p>
+                  <Link href="tel:197-90-56-780">Fax : +92-51-11234567</Link>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-xl-3">
+            <div className="contact-m__single topy-tilt fade-top">
+              <div className="thumb">
+                <Image src={mail} alt="Image" />
+              </div>
+              <div className="content">
+                <h4>Mail Address</h4>
+                <p>
+                  <Link href="mailto:info.company@gmail.com">
+                    Info.realitai@gmail.com
+                  </Link>
+                </p>
+                <p>
+                  <Link href="mailto:info.company@gmail.com">
+                    Info@realitai.com
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-xl-3">
+            <div className="contact-m__single topy-tilt fade-top">
+              <div className="thumb">
+                <Image src={location} alt="Image" />
+              </div>
+              <div className="content">
+                <h4>Our Location</h4>
+                <p>
+                  <Link
+                    href="https://www.google.com/maps/place/DHA+Phase+II,+Islamabad,+Islamabad+Capital+Territory,+Pakistan/@33.5276785,73.1320844,14z/data=!3m1!4b1!4m6!3m5!1s0x38dfed8dabf00f19:0xb812318c3025e7a!8m2!3d33.5368355!4d73.1644628!16s%2Fg%2F1vb9bfhl?entry=ttu"
+                    target="_blank"
+                  >
+                    DHA Phase-2 Islamabad, Pakistan
+                  </Link>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-xl-3">
+            <div className="contact-m__single topy-tilt fade-top">
+              <div className="thumb">
+                <Image src={time} alt="Image" />
+              </div>
+              <div className="content">
+                <h4>Office Hour</h4>
+                <p>Sun - Thu 09 am - 06pm</p>
+                <p>Fri - Sat 4 pm - 10pm</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <div className="map-wrapper">
+              <div className="row gaper">
+                <div className="col-12 col-lg-6">
+                  <div className="contact__map fade-top">
+                    <iframe
+                      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20342.411046372905!2d-74.16638039276373!3d40.719832743885284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1649562691355!5m2!1sen!2sbd"
+                      width="100"
+                      height="600"
+                      style={{ border: "0px" }}
+                      allowFullScreen
+                      loading="lazy"
+                      referrerPolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                  </div>
+                </div>
+                <div className="col-12 col-lg-6">
+                  <div className="contact-main__form  fade-top">
+                    <h3>Leave A Message</h3>
+                    <form
+                      action="#"
+                      method="post"
+                      className="section__content-cta"
+                    >
+                      <div className="group-wrapper">
+                        <div className="group-input ">
+                          <input
+                            type="text"
+                            name="contact-name"
+                            id="contactName"
+                            placeholder="Name"
+                          />
+                        </div>
+                        <div className="group-input ">
+                          <input
+                            type="email"
+                            name="contact-email"
+                            id="contactEmail"
+                            placeholder="Email"
+                          />
+                        </div>
+                      </div>
+                      <div className="group-input drt">
+                        <select className="subject">
+                          <option data-display="Subject">Subject</option>
+                          <option value="1">Account</option>
+                          <option value="2">Service</option>
+                          <option value="3">Pricing</option>
+                          <option value="4">Support</option>
+                        </select>
+                      </div>
+                      <div className="group-input ">
+                        <textarea
+                          name="contact-message"
+                          id="contactMessage"
+                          placeholder="Message"
+                        ></textarea>
+                      </div>
+                      <div className="form-cta justify-content-start">
+                        <button type="submit" className="btn btn--primary">
+                          Send Message
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContactMain;
